bf304c955a179370336d09ca3e0b644f995761f9
Chris Caruso <carusocr@ldc.upenn.edu>
2017-04-23 15:53:40 -0700

    mmttt

--- poc/mmttt.py
+++ poc/mmttt.py
@@ -5,59 +5,6 @@ Mini-max Tic-Tac-Toe Player
 import poc_ttt_gui
 import poc_ttt_provided as provided
 
-class TTTBoard:
-    """
-    Class to represent a Tic-Tac-Toe board.
-    """
-
-    def __init__(self, dim, reverse = False, board = None):
-        """
-        Initialize the TTTBoard object with the given dimension and 
-        whether or not the game should be reversed.
-        """
-            
-    def __str__(self):
-        """
-        Human readable representation of the board.
-        """
-
-    def get_dim(self):
-        """
-        Return the dimension of the board.
-        """
-    
-    def square(self, row, col):
-        """
-        Returns one of the three constants EMPTY, PLAYERX, or PLAYERO 
-        that correspond to the contents of the board at position (row, col).
-        """
-
-    def get_empty_squares(self):
-        """
-        Return a list of (row, col) tuples for all empty squares
-        """
-
-    def move(self, row, col, player):
-        """
-        Place player on the board at position (row, col).
-        player should be either the constant PLAYERX or PLAYERO.
-        Does nothing if board square is not empty.
-        """
-
-    def check_win(self):
-        """
-        Returns a constant associated with the state of the game
-            If PLAYERX wins, returns PLAYERX.
-            If PLAYERO wins, returns PLAYERO.
-            If game is drawn, returns DRAW.
-            If game is in progress, returns None.
-        """
-            
-    def clone(self):
-        """
-        Return a copy of the board.
-        """
-
 # Set timeout, as mini-max can take a long time
 import codeskulptor
 codeskulptor.set_timeout(60)
@@ -74,10 +21,23 @@ def mm_move(board, player):
     Returns a tuple with two elements.  The first element is the score
     of the given board and the second element is the desired move as a
     tuple, (row, col).
-    Remember to immediately return a move if its value is
-    1/-1 (maximizing vs minimizing)!
     """
-    return 0, (-1, -1)
+    # base case = no moves to make
+    if board.check_win() != None:
+        return SCORES[board.check_win()], (-1,-1)
+    selected = (-1, (-1,-1))
+    for square in board.get_empty_squares():
+        copy_board = board.clone()
+        copy_board.move(square[0], square[1], player)
+        #switch player, recurse
+        score, location = mm_move(copy_board, provided.switch_player(player))
+        if score * SCORES[player] == 1: # good move
+            return score, square
+        elif score * SCORES[player] == 0: #better than worst
+            selected = (score, square)
+        elif selected[0] == -1: # still stuck with crap
+            selected = (-1, square)
+    return selected[0] * SCORES[player], selected[1]
 
 def move_wrapper(board, player, trials):
     """
@@ -94,5 +54,5 @@ def move_wrapper(board, player, trials):
 # testing to save time.
 
 # provided.play_game(move_wrapper, 1, False)        
-poc_ttt_gui.run_gui(3, provided.PLAYERO, move_wrapper, 1, False)
+#poc_ttt_gui.run_gui(3, provided.PLAYERO, move_wrapper, 1, False)
 
