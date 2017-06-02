50d5b8417de52c3d66c0532145226a7354954f36
Chris Caruso <carusocr@ldc.upenn.edu>
2017-06-02 16:12:05 -0700

    adding performance graph

---
+++ algos/alg3_application.py
@@ -0,0 +1,28 @@
+import random
+import urllib2
+import matplotlib.pyplot as plt
+import time
+import timeit
+
+from alg_proj3 import *
+
+def gen_random_clusters(num_clusters):
+  """
+  creates a list of clusters where each cluster in this list corresponds to one randomly generated point in the square with corners (+/-1,+/-1).
+  """
+  return [alg_cluster.Cluster(set([]),random.uniform(-1,1),random.uniform(-1,1),0,0) for x in range(num_clusters)]
+
+def measure_performance(n, func):
+  ran_clus = gen_random_clusters(n)
+  func(ran_clus)
+
+def plot_performance():
+  xs = range(2, 200)
+  scp = [timeit.timeit(lambda: measure_performance(n, slow_closest_pair),number=1) for n in xs]
+  fcp = [timeit.Timer(lambda: measure_performance(n, fast_closest_pair)).timeit(number=1) for n in xs]
+  plt.plot(xs, scp, '-r', label='sloooow')
+  plt.plot(xs, fcp, '-b', label='FAST')
+  plt.tight_layout()
+  plt.show()
+  
+plot_performance()
--- algos/alg_proj3.py
+++ algos/alg_proj3.py
@@ -117,15 +117,6 @@ def closest_pair_strip(cluster_list, horiz_center, half_width):
     
 ######################################################################
 # Code for hierarchical clustering
-"""
-For the second part of the Project, your task is to implement hierarchical clustering and k-means clustering. In particular, you should implement the following two functions:
-
-𝚑𝚒𝚎𝚛𝚊𝚛𝚌𝚑𝚒𝚌𝚊𝚕_𝚌𝚕𝚞𝚜𝚝𝚎𝚛𝚒𝚗𝚐(𝚌𝚕𝚞𝚜𝚝𝚎𝚛_𝚕𝚒𝚜𝚝, 𝚗𝚞𝚖_𝚌𝚕𝚞𝚜𝚝𝚎𝚛𝚜) - Takes a list of 𝙲𝚕𝚞𝚜𝚝𝚎𝚛 objects and applies hierarchical clustering as described in the pseudo-code HierarchicalClustering from Homework 3 to this list of clusters. This clustering process should proceed until 𝚗𝚞𝚖_𝚌𝚕𝚞𝚜𝚝𝚎𝚛𝚜 clusters remain. The function then returns this list of clusters.
-Note that your implementation of lines 5-6 in the pseudo-code need not match the pseudo-code verbatim. In particular, merging one cluster into the other using 𝚖𝚎𝚛𝚐𝚎_𝚌𝚕𝚞𝚜𝚝𝚎𝚛𝚜 and then removing the other cluster is fine. Note that, for this function, mutating 𝚌𝚕𝚞𝚜𝚝𝚎𝚛_𝚕𝚒𝚜𝚝 is allowed to improve performance.
-
-𝚔𝚖𝚎𝚊𝚗𝚜_𝚌𝚕𝚞𝚜𝚝𝚎𝚛𝚒𝚗𝚐(𝚌𝚕𝚞𝚜𝚝𝚎𝚛_𝚕𝚒𝚜𝚝, 𝚗𝚞𝚖_𝚌𝚕𝚞𝚜𝚝𝚎𝚛𝚜, 𝚗𝚞𝚖_𝚒𝚝𝚎𝚛𝚊𝚝𝚒𝚘𝚗𝚜) - Takes a list of 𝙲𝚕𝚞𝚜𝚝𝚎𝚛 objects and applies k-means clustering as described in the pseudo-code KMeansClustering from Homework 3 to this list of clusters. This function should compute an initial list of clusters (line 2 in the pseudo-code) with the property that each cluster consists of a single county chosen from the set of the 𝚗𝚞𝚖_𝚌𝚕𝚞𝚜𝚝𝚎𝚛 counties with the largest populations. The function should then compute 𝚗𝚞𝚖_𝚒𝚝𝚎𝚛𝚊𝚝𝚒𝚘𝚗𝚜 of k-means clustering and return this resulting list of clusters.
-As you implement KMeansClustering, here are a several items to keep in mind. In line 4, you should represent an empty cluster as a 𝙲𝚕𝚞𝚜𝚝𝚎𝚛 object whose set of counties is empty and whose total population is zero. The cluster centers μf, computed by lines 2 and 8-9, should stay fixed as lines 5-7 are executed during one iteration of the outer loop. To avoid modifying these values during execution of lines 5-7, you should consider storing these cluster centers in a separate data structure. Line 7 should be implemented using the 𝚖𝚎𝚛𝚐𝚎_𝚌𝚕𝚞𝚜𝚝𝚎𝚛𝚜 method from the 𝙲𝚕𝚞𝚜𝚝𝚎𝚛 class. 𝚖𝚎𝚛𝚐𝚎_𝚌𝚕𝚞𝚜𝚝𝚎𝚛𝚜 will automatically update the cluster centers to their correct locations based on the relative populations of the merged clusters.
-"""
 
 
 def hierarchical_clustering(cluster_list, num_clusters):
@@ -137,13 +128,13 @@ def hierarchical_clustering(cluster_list, num_clusters):
     Output: List of clusters whose length is num_clusters
     """
     while len(cluster_list) > num_clusters:
+        # Muuuuuutation!!!!!!
         cluster_list.sort(key = lambda cluster: cluster.horiz_center())
-        _, cluster_i, cluster_j = fast_closest_pair(cluster_list)
+        dist, cluster_i, cluster_j = fast_closest_pair(cluster_list)
         cluster_list[cluster_i].merge_clusters(cluster_list[cluster_j])
-        del cluster_list[cluster_j]  
+        del cluster_list[cluster_j]
     return cluster_list
-    
-    return []
+
 
 
 ######################################################################
@@ -157,21 +148,41 @@ def kmeans_clustering(cluster_list, num_clusters, num_iterations):
     
     Input: List of clusters, integers number of clusters and number of iterations
     Output: List of clusters whose length is num_clusters
+    Plain English version:
+    
+    1.Initialize old cluster using large population counties 
+    2.For number of iterations 
+      3.Initialize the new clusters to be empty 
+      4.For each county 
+          5.Find the old cluster center that is closest 
+          6.Add the county to the corresponding new cluster 
+      7.Set old clusters equal to new clusters 
+    8.Return the new clusters
     """
+    # Initialize old cluster using large population counties
+    cluster = [dummy_cluster.copy() for dummy_cluster in cluster_list]
+    cluster.sort(key = lambda cluster_list: cluster_list.total_population(), reverse = True)
+    center = [dummy_cluster for dummy_cluster in cluster[:num_clusters]]
+    for dummy_iter in range(num_iterations):
+        # Initialize new clusters to be empty Cluster objects
+        new_clusters = [alg_cluster.Cluster(set([]),0,0,0,0) for x in range(num_clusters)]
+        # For each county
+        for dummy_idx in range(len(cluster_list)):
+            distance = float('inf')
+            # Find the old cluster center that is closest
+            for dummy_center in range(num_clusters):
+                newdist = cluster_list[dummy_idx].distance(center[dummy_center])
+                if newdist < distance:
+                    distance = newdist
+                    cluster_sel = dummy_center
+            # Add county to corresponding new cluster
+            #copy if empty, otherwise merge
+            if new_clusters[cluster_sel] == []:
+                new_clusters[cluster_sel] = cluster_list[dummy_idx].copy()
+            else:
+                new_clusters[cluster_sel].merge_clusters(cluster_list[dummy_idx])
+        for dummy_center in range(num_clusters):
+            # Set old clusters equal to new clusters
+            center[dummy_center] = new_clusters[dummy_center].copy()
 
-    # position initial clusters at the location of clusters with largest populations
-            
-    return []
-
-#print slow_closest_pair([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0)])
-#print closest_pair_strip([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 0, 1, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 1, 1, 0)], 0.5, 1.0)
-#print closest_pair_strip([alg_cluster.Cluster(set([]), 1.0, 1.0, 1, 0),alg_cluster.Cluster(set([]), 1.0, 5.0, 1, 0), alg_cluster.Cluster(set([]),1.0, 4.0, 1, 0), alg_cluster.Cluster(set([]), 1.0, 7.0, 1, 0)], 1.0, 3.0) 
-#expected one of the tuples in set([(1.0, 1, 2)]) but received (1.0, 2, 1)
-fast_closest_pair([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0), alg_cluster.Cluster(set([]), 2, 0, 1, 0), alg_cluster.Cluster(set([]), 3, 0, 1, 0)])
-#expected one of the tuples in set([(0.089442719099999998, 1, 4)]) but received (0.18248287590894655, 1, 2)
-print closest_pair_strip([alg_cluster.Cluster(set([]), 0.32, 0.16, 1, 0), alg_cluster.Cluster(set([]), 0.39, 0.4, 1, 0), alg_cluster.Cluster(set([]), 0.54, 0.8, 1, 0), alg_cluster.Cluster(set([]), 0.61, 0.8, 1, 0), alg_cluster.Cluster(set([]), 0.76, 0.94, 1, 0)], 0.46500000000000002, 0.070000000000000007)
-print fast_closest_pair([alg_cluster.Cluster(set([]), 0.02, 0.39, 1, 0), alg_cluster.Cluster(set([]), 0.19, 0.75, 1, 0), alg_cluster.Cluster(set([]), 0.35, 0.03, 1, 0), alg_cluster.Cluster(set([]), 0.73, 0.81, 1, 0), alg_cluster.Cluster(set([]), 0.76, 0.88, 1, 0), alg_cluster.Cluster(set([]), 0.78, 0.11, 1, 0)])
-#expected one of the tuples in set([(0.076157731058639044, 3, 4)]) 
-#but received (0.076157731058639044, 3.0, 4.0) (Exception: Invalid Types)
-#Incompatible types being compared.  Expected 3  <type 'int'> but received 3.0
-#<type 'float'>
+    return new_clusters
